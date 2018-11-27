558d417094d0b9af465bc9ef92c23ed9ba2c514a
futorio <fd.cvb@yandex.ru>
2018-11-28 04:29:25 +0500

    split method find_task_by to methods: find_task_by_number, find_tasks_by_tags, find_tasks_by_name

--- bot/tasksdbs.py
+++ bot/tasksdbs.py
@@ -9,7 +9,7 @@ class TasksDatabase(object):
         try:
             self.base_connection = sqlite3.connect(f"file:{path}?mode=rw", uri=True)
         except sqlite3.OperationalError:
-            raise OSError(f"wrong path: {path}")
+            raise OSError("database does not exist")
         else:
             self.base_cursor = self.base_connection.cursor()
 
@@ -31,39 +31,74 @@ class TasksDatabase(object):
 
         return sql_response.fetchall()
 
-    def find_tasks_by(self, **parameter) -> list:
+    @staticmethod
+    def get_tasks(sql_response: list) -> list:
         """
-        find all tasks with current number or name or tag
-        Example: find_tasks_by(name="Найти") return list with
-        all "task" objects with current substring "Найти" in the name field
-        :param parameter: number: int, name: str, tag: str
+        Create list of Task object
+        :param sql_response: list of tuple value from database
+        :return: list of task
+        """
+
+        if len(sql_response) > 0:
+            return [Task(*args) for args in sql_response]
+        else:
+            return []
+
+    def find_tasks_by_name(self, name: str) -> list:
+        """
+        Find all tasks with current name substring
+        :param name: name substring
         :return: list of tasks
         """
 
-        sql_requests = {
-            "tag": "SELECT * FROM tasks WHERE tags LIKE ?",
-            "number": "SELECT * FROM tasks WHERE number=?",
-            "name": "SELECT * FROM tasks WHERE name LIKE ?"
-        }
+        sql_request = "SELECT * FROM tasks WHERE name LIKE ?"
+        sql_response = self.get_sql_response(self.base_cursor,
+                                             sql_request,
+                                             ('%' + name + '%',)
+                                             )
 
-        parameter_name, parameter_value = tuple(parameter.items())[0]
+        return self.get_tasks(sql_response)
 
-        if parameter_name not in sql_requests.keys():
-            raise AttributeError
+    def find_task_by_number(self, number: int) -> list:
+        """
+        Find all tasks with current number
+        :param number: task index. Starts with 1
+        :return: list of tasks
+        """
 
-        if parameter_name is not "number":
-            parameter_value = '%' + parameter_value + '%'
+        sql_request = "SELECT * FROM tasks WHERE number=?"
+        sql_response = self.get_sql_response(self.base_cursor,
+                                             sql_request,
+                                             (int(number),)
+                                             )
+
+        return self.get_tasks(sql_response)
+
+    def find_tasks_by_tags(self, tags: list, mode: str = "or") -> list:
+        """
+        Find all tasks with current tags.
+        Mode "or" mean that at least one tag is in the task.
+        Mode "and" mean that all tag must be in the task.
+        :param tags: list of tags
+        :param mode: find mode. Default value "or"
+        :return: list of tasks
+        """
+
+        if mode.lower() not in ["or", "and"]:
+            raise AttributeError(f"mode must be \"or\" or \"and\"")
+
+        logic_separator = ' ' + mode.upper() + ' '
+        params_sql_requests = logic_separator.join(['?' for tag in tags])
+        base_sql_request = "SELECT * FROM tasks WHERE tags LIKE "
+
+        sql_request = base_sql_request + params_sql_requests
+        sql_params = tuple(['%'+param+'%' for param in tags])
 
         sql_response = self.get_sql_response(self.base_cursor,
-                                             sql_requests[parameter_name],
-                                             (parameter_value,)
+                                             sql_request,
+                                             sql_params,
                                              )
-        tasks = []
-        if len(sql_response) > 0:
-            for args in sql_response:
-                tasks.append(Task(*args))
-
-        return tasks
+        return self.get_tasks(sql_response)
 
     def add_task(self, task: Task):
         """
@@ -77,7 +112,7 @@ class TasksDatabase(object):
 
         self.base_cursor.execute(
             sql_request,
-            (max_index+1, task.name, task.url,)
+            (max_index + 1, task.name, task.url,)
         )
         self.base_connection.commit()
 
