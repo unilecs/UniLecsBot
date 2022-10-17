3f62675c4dcebdede52986cf3e06a463ed2f7896
Albert Davletov <albert.m.davletov@gmail.com>
2022-10-16 23:23:41 -0700

    Send feedback & solution fixes + code prettify (#22)

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import time
 from random import randint
 
 def get_task_by_number(tasks, number):
@@ -13,3 +14,14 @@ def get_random_task(tasks):
         return get_task_by_number(tasks, rand)
     else:
         return None
+
+def get_feedback_form(message):
+    time_at_now = time.strftime("%H:%M:%S %Y.%m.%d", time.localtime())
+    form = """Feedback from {0} - @{1} ({2});\nDate: {3};\nText: {4}""".format(
+        message.from_user.first_name,
+        message.from_user.username,
+        message.from_user.id,
+        time_at_now,
+        message.text,
+    )
+    return form
